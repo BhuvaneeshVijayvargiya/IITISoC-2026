26374145bf69f00050d22ebfe825c585282fab18
Tanush Bansal <tanushbansal37@gmail.com>
2026-06-12 10:11:48 +0530

    changed file loc

--- input.py
+++ input.py
@@ -2,14 +2,14 @@ import pandas as pd
 from packages import Package
 from ULDs import UlDs
 def pack():
-    df=pd.read_csv(r"C:\Users\Tanush Bansal\OneDrive\Documents\IITISoC\Input_Tests\packages1.csv")
+    df=pd.read_csv(r"")
     packlist=[]
     for ind,row in df.iterrows():
         p=Package(row["Package_ID"],row["Length"],row["Width"],row["Height"],row["Weight"],row["Category"],row["Delay Cost"])
         packlist.append(p)
     return packlist
 def ulds():
-    df=pd.read_csv(r"C:\Users\Tanush Bansal\OneDrive\Documents\IITISoC\Input_Tests\ulds1.csv")
+    df=pd.read_csv(r"")
     uldlist=[]
     for ind,row in df.iterrows():
         u=UlDs(row["ULD_ID"],row["Length"],row["Width"],row["Height"],row["MaxWeight"])
