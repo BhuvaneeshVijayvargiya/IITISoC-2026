15c77e07856c391a44d3cdc00d131d32d1e00f16
Tanush Bansal <tanushbansal37@gmail.com>
2026-06-14 17:27:41 +0530

    add comments

--- packages.py
+++ packages.py
@@ -8,7 +8,11 @@ class Package:
         self.height=height
         self.weight=weight
         self.category=category
-        self.delay_cost=delay_cost
+        if(self.category=="Economy"):
+            self.delay_cost=delay_cost
+        else:
+            self.delay_cost=delay_cost
+            self.delay_cost=None
 
     def volume(self):
 
@@ -16,3 +20,7 @@ class Package:
 
     def __str__(self):
         return f"{self.id}:({self.length},{self.width},{self.height})"
+
+#this is the Package class with id,dimension,weight,category,delay cost attributes.
+#volume function returns volume of package
+#__str__ is a dunder method that returns a string of id,length,width,height of package separated by commas(will be used in output)
