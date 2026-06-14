9266d346383dc7a4574d80fa82f28f19be95881e
Tanush Bansal <tanushbansal37@gmail.com>
2026-06-14 17:39:32 +0530

    started..

--- solver.py
+++ solver.py
@@ -1,2 +1,8 @@
-from ortools.linear_solver import pywraplp
+"""Here the code for finding the space for the next space for the package will come"""
 import numpy as np
+from input import pack,ulds
+
+packages_list=pack()
+ulds_list=ulds()
+
+
