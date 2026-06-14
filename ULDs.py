d153e96cfead759335936784daa03251b260110e
Tanush Bansal <tanushbansal37@gmail.com>
2026-06-14 17:12:45 +0530

    update with feasibility variables

--- ULDs.py
+++ ULDs.py
@@ -1,3 +1,4 @@
+import numpy as np
 class UlDs:
 
     def __init__(self, id, length, width,height, max_weight):
@@ -7,7 +8,14 @@ class UlDs:
         self.width=width
         self.height=height
         self.max_weight=max_weight
+        self.z=np.zeros((length,width))
+        self.currweight=0
+        self.extr=[(0,0,0)]
+        self.packaged=[]
 
     def volume(self):
 
         return (self.length*self.width*self.height)
+    
+
+    
