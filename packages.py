41dcc29ab106566cc06f5263a9c3d631d4268e10
Tanush Bansal <tanushbansal37@gmail.com>
2026-06-15 17:20:03 +0530

    add new things

--- packages.py
+++ packages.py
@@ -1,3 +1,4 @@
+from itertools import permutations
 class Package:
 
     def __init__(self, id, length, width, height,weight, category, delay_cost):
@@ -13,6 +14,9 @@ class Package:
         else:
             self.delay_cost=delay_cost
             self.delay_cost=None
+        self.uld=None
+        self.pos=None
+        self.ori=None
 
     def volume(self):
 
@@ -20,7 +24,13 @@ class Package:
 
     def __str__(self):
         return f"{self.id}:({self.length},{self.width},{self.height})"
+    
+    def orie(self):
+        dims=(self.length,self.width,self.height)
+        return list(set(permutations(dims,3)))
 
 #this is the Package class with id,dimension,weight,category,delay cost attributes.
+#uld,pos,ori will store the final uld,position coordinates(left bottom front corner) and orientation of the package when packed.
 #volume function returns volume of package
 #__str__ is a dunder method that returns a string of id,length,width,height of package separated by commas(will be used in output)
+#orie function generated all the 6 possible orientations.
