89be9b90e84c54e1bcd83fd4e3b8f4db4a06912f
Tanush Bansal <tanushbansal37@gmail.com>
2026-06-30 11:58:43 +0530

    add for demo

---
+++ visualize.py
@@ -0,0 +1,64 @@
+import plotly.graph_objects as go
+
+
+def visualize_ulds(ulds):
+
+    for uld in ulds:
+
+        fig=go.Figure()
+
+        for package in uld.placed_packages:
+
+            x,y,z = package.pos
+            l,w,h = package.ori
+
+            fig.add_trace(go.Scatter3d(
+                x=[x, x+l, x+l, x, x],
+                y=[y, y, y+w, y+w, y],
+                z=[z, z, z, z, z],
+                mode="lines",
+                line=dict(color="blue",width=3),
+            ))
+
+            fig.add_trace(go.Scatter3d(
+                x=[x, x+l, x+l, x, x],
+                y=[y, y, y+w, y+w, y],
+                z=[z+h, z+h, z+h, z+h, z+h],
+                mode="lines",
+                line=dict(color="blue", width=3),
+            ))
+
+            corners = [
+                (x,y),
+                (x+l,y),
+                (x+l,y+w),
+                (x,y+w)
+            ]
+
+            for cx,cy in corners:
+                fig.add_trace(go.Scatter3d(
+                    x=[cx,cx],
+                    y=[cy,cy],
+                    z=[z,z+h],
+                    mode="lines",
+                    line=dict(color="blue",width=3),
+                ))
+
+            fig.add_trace(go.Scatter3d(
+                x=[x+l/2],
+                y=[y+w/2],
+                z=[z+h/2],
+                mode="text",
+                text=[package.id],
+            ))
+
+        fig.update_layout(
+            title=f"{uld.id}",
+            scene=dict(
+                xaxis_title="Length",
+                yaxis_title="Width",
+                zaxis_title="Height",
+            )
+        )
+
+        fig.show()
