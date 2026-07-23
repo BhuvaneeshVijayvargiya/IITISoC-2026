dc43b7e005fd1e64aa118e2e6c54de5625bbb858
Bhuvaneesh Vijayvargiya <bhuvaneeshvijayvargiya@gmail.com>
2026-07-23 15:31:43 +0530

    Implementing Beam Search

--- run.py
+++ run.py
@@ -2,17 +2,20 @@
 #python run.py
 
 from input import load
-from solver import solve
+from solver import solve_beam
 from output import write_single
 from pct_store import save_pct
+from beam_store import save_beam_branches
 
 packages, ulds, K = load("sample_input.csv")
-result, pct_store = solve(packages, ulds, K)
-write_single(result, out_dir="output")
-save_pct(pct_store, out_dir="output")
+final_results, best_branch = solve_beam(packages, ulds, K, beam_width=3)
 
-print("Done! Cost:", result["summary"]["total_cost"])
-print("Feasible:", result["summary"]["is_feasible"])
+write_single(best_branch["result"], out_dir="output")
+save_pct(best_branch["pct_log"], out_dir="output")
+save_beam_branches(final_results, best_branch, out_dir="output")
+
+print("Done! Best cost:", best_branch["result"]["summary"]["total_cost"])
+print("Feasible:", best_branch["result"]["summary"]["is_feasible"])
 
 from visualize import visualize_ulds
-visualize_ulds(ulds)
+visualize_ulds(best_branch["ulds"])
