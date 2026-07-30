9a12566b09dc15879d57c414d4ad3554b93f7635
ArushVAgrawal <arushvagrawal@gmail.com>
2026-07-31 00:07:05 +0530

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -1,65 +1,66 @@
 #this will help visualize the placement of the packages
 import plotly.graph_objects as go
 
+def create_figure(uld):
+    fig = go.Figure()
 
-def visualize_ulds(ulds):
+    for package in uld.placed_packages:
 
-    for uld in ulds:
+        x, y, z = package.pos
+        l, w, h = package.ori
 
-        fig=go.Figure()
+        fig.add_trace(go.Scatter3d(
+            x=[x, x+l, x+l, x, x],
+            y=[y, y, y+w, y+w, y],
+            z=[z, z, z, z, z],
+            mode="lines",
+            line=dict(color="blue", width=3),
+        ))
 
-        for package in uld.placed_packages:
+        fig.add_trace(go.Scatter3d(
+            x=[x, x+l, x+l, x, x],
+            y=[y, y, y+w, y+w, y],
+            z=[z+h, z+h, z+h, z+h, z+h],
+            mode="lines",
+            line=dict(color="blue", width=3),
+        ))
 
-            x,y,z = package.pos
-            l,w,h = package.ori
+        corners = [
+            (x, y),
+            (x+l, y),
+            (x+l, y+w),
+            (x, y+w),
+        ]
 
+        for cx, cy in corners:
             fig.add_trace(go.Scatter3d(
-                x=[x, x+l, x+l, x, x],
-                y=[y, y, y+w, y+w, y],
-                z=[z, z, z, z, z],
-                mode="lines",
-                line=dict(color="blue",width=3),
-            ))
-
-            fig.add_trace(go.Scatter3d(
-                x=[x, x+l, x+l, x, x],
-                y=[y, y, y+w, y+w, y],
-                z=[z+h, z+h, z+h, z+h, z+h],
+                x=[cx, cx],
+                y=[cy, cy],
+                z=[z, z+h],
                 mode="lines",
                 line=dict(color="blue", width=3),
             ))
 
-            corners = [
-                (x,y),
-                (x+l,y),
-                (x+l,y+w),
-                (x,y+w)
-            ]
+        fig.add_trace(go.Scatter3d(
+            x=[x+l/2],
+            y=[y+w/2],
+            z=[z+h/2],
+            mode="text",
+            text=[package.id],
+        ))
 
-            for cx,cy in corners:
-                fig.add_trace(go.Scatter3d(
-                    x=[cx,cx],
-                    y=[cy,cy],
-                    z=[z,z+h],
-                    mode="lines",
-                    line=dict(color="blue",width=3),
-                ))
+    fig.update_layout(
+        title=f"{uld.id}",
+        scene=dict(
+            xaxis_title="Length",
+            yaxis_title="Width",
+            zaxis_title="Height",
+        ),
+    )
 
-            fig.add_trace(go.Scatter3d(
-                x=[x+l/2],
-                y=[y+w/2],
-                z=[z+h/2],
-                mode="text",
-                text=[package.id],
-            ))
+    return fig
+def visualize_ulds(ulds):
 
-        fig.update_layout(
-            title=f"{uld.id}",
-            scene=dict(
-                xaxis_title="Length",
-                yaxis_title="Width",
-                zaxis_title="Height",
-            )
-        )
-
+    for uld in ulds:
+        fig = create_figure(uld)
         fig.show()
