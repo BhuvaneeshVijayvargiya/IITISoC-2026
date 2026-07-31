618918c06e6445d82deedb52cbbb9c2b8ae285c2
Tanush Bansal <tanushbansal37@gmail.com>
2026-07-31 10:36:58 +0530

    ...

--- solver.py
+++ solver.py
@@ -79,11 +79,11 @@ def score_node(node):
     score=0.0
     if uld.has_priority and pkg.package_type=="Priority":
         score+=1000
-    cog_deviation=compute_cog_deviation(node)
-    score-=15*cog_deviation
-    resulting_volume= uld.used_volume()+(node.l * node.w * node.h)
-    resulting_utilization = resulting_volume / uld.volume() if uld.volume() > 0 else 0.0
-    score+=30*resulting_utilization
+    cog=compute_cog_deviation(node)
+    score-=15*cog
+    vol= uld.used_volume()+(node.l * node.w * node.h)
+    util = vol / uld.volume() if uld.volume() > 0 else 0.0
+    score+=30*util
 
 
     return score
